8b9d897215b8825337f1c7d7c69507e85c30e84e
Ha Quanq <quangha0120010@gmail.com>
2025-11-18 01:32:14 +0700

    Server: implement JWTSubject to User model

--- apps/server/app/Modules/Auth/Models/User.php
+++ apps/server/app/Modules/Auth/Models/User.php
@@ -2,19 +2,31 @@
 
 namespace App\Modules\Auth\Models;
 
-use App\Abstracts\BaseModel;
 use App\Modules\Interview\Models\Interview;
 use App\Modules\Interview\Models\InterviewEvaluation;
 use App\Modules\Interview\Models\InterviewInterviewer;
 use App\Modules\Position\Models\Position;
 use App\Modules\Proposal\Models\Proposal;
 use App\Modules\Recruitment\Models\Recruitment;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends BaseModel
+class User extends Authenticatable implements JWTSubject
 {
+    use HasFactory;
+
+    protected $table = "user";
+
+    protected $casts = [
+        "password" => "hashed",
+    ];
+
+    protected $guarded = ["id", "createdAt", "updatedAt", "retiredAt", "suspensionStartAt"];
+
     public function position(): BelongsTo
     {
         return $this->belongsTo(Position::class);
@@ -43,5 +55,15 @@ class User extends BaseModel
     public function evaluatedInterviews(): HasMany
     {
         return $this->hasMany(InterviewEvaluation::class);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
