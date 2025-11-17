eeca30af3096e97e994ae47a737e080327748931
Ha Quanq <quangha0120010@gmail.com>
2025-11-17 16:20:00 +0700

    Server: add controller error responses

--- apps/server/app/Abstracts/BaseController.php
+++ apps/server/app/Abstracts/BaseController.php
@@ -7,10 +7,9 @@ use Symfony\Component\HttpFoundation\Response;
 
 abstract class BaseController
 {
-    public function notFoundResponse(string $message = "Resource not found."): JsonResponse
-    {
-        return response()->json(["message" => $message], Response::HTTP_NOT_FOUND);
-    }
+    /* -------------------------------------------------------------------------- */
+    /*                              SUCCESSFUL RESPONSE                           */
+    /* -------------------------------------------------------------------------- */
 
     public function okResponse(array|object $data): JsonResponse
     {
@@ -25,5 +24,29 @@ abstract class BaseController
     public function noContentResponse(): JsonResponse
     {
         return response()->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+    /* -------------------------------------------------------------------------- */
+    /*                               CLIENT ERROR RESPONSE                        */
+    /* -------------------------------------------------------------------------- */
+
+    public function badRequestResponse(string $message = "Bad request."): JsonResponse
+    {
+        return response()->json(["message" => $message], Response::HTTP_BAD_REQUEST);
+    }
+
+    public function unauthorizedResponse(string $message = "Unauthorized."): JsonResponse
+    {
+        return response()->json(["message" => $message], Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function forbiddenResponse(string $message = "Forbidden."): JsonResponse
+    {
+        return response()->json(["message" => $message], Response::HTTP_FORBIDDEN);
+    }
+
+    public function notFoundResponse(string $message = "Resource not found."): JsonResponse
+    {
+        return response()->json(["message" => $message], Response::HTTP_NOT_FOUND);
     }
 }
