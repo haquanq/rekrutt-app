bd7ad311b9a16fa5586227fd39645f80240f320b
Ha Quanq <quangha0120010@gmail.com>
2025-11-18 01:30:52 +0700

    Server: add field to CandidateFactory

--- apps/server/database/factories/CandidateFactory.php
+++ apps/server/database/factories/CandidateFactory.php
@@ -15,12 +15,10 @@ class CandidateFactory extends Factory
         return [
             "first_name" => fake()->firstName(),
             "last_name" => fake()->lastName(),
-            "date_of_birth" => fake()
-                ->dateTimeBetween("-45years", "-21years")
-                ->format("Y M d"),
+            "date_of_birth" => fake()->dateTimeBetween("-45years", "-21years")->format("Y M d"),
             "address" => fake()->address(),
             "email" => fake()->unique()->safeEmail(),
-            "phoneNumber" => fake()->unique()->numerify("##########"),
+            "phone_number" => fake()->unique()->numerify("##########"),
             "status" => CandidateStatus::NEW,
             "hiring_source_id" => random_int(1, 9),
             "created_at" => Carbon::now(),
