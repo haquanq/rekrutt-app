60c6d11670d5df3e3729c2548d7e57a2a07a0d6e
Ha Quanq <quangha0120010@gmail.com>
2025-11-18 01:31:25 +0700

    Server: update UserSeeder

--- apps/server/database/seeders/UserSeeder.php
+++ apps/server/database/seeders/UserSeeder.php
@@ -3,49 +3,53 @@
 namespace Database\Seeders;
 
 use App\Modules\Auth\Enums\UserRole;
+use Carbon\Carbon;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        $users = [];
-
         $recruiterIds = [41, 41, 41, 41, 41, 43, 43, 43];
 
         foreach ($recruiterIds as $id) {
-            $newUser = UserFactory::new()
-                ->create([
-                    "position_id" => $id,
-                    "role" => UserRole::RECRUITER,
-                ])
-                ->toArray();
-            array_push($users, $newUser);
+            UserFactory::new()->create([
+                "position_id" => $id,
+                "role" => UserRole::RECRUITER,
+            ]);
         }
 
         $managerIds = [8, 10, 11, 12, 25, 29, 31, 34, 37, 39, 42, 44];
 
         foreach ($managerIds as $id) {
-            $newUser = UserFactory::new()
-                ->create([
-                    "position_id" => $id,
-                    "role" => UserRole::MANAGER,
-                ])
-                ->toArray();
-            array_push($users, $newUser);
+            UserFactory::new()->create([
+                "position_id" => $id,
+                "role" => UserRole::MANAGER,
+            ]);
         }
 
         $executiveIds = [9, 14, 20, 28, 30, 36, 45, 50];
 
         foreach ($executiveIds as $id) {
-            $newUser = UserFactory::new()
-                ->create([
-                    "position_id" => $id,
-                    "role" => UserRole::EXECUTIVE,
-                ])
-                ->toArray();
-            array_push($users, $newUser);
+            UserFactory::new()->create([
+                "position_id" => $id,
+                "role" => UserRole::EXECUTIVE,
+            ]);
         }
+
+        UserFactory::new()->create([
+            "first_name" => "Ha",
+            "last_name" => "Quang",
+            "email" => "hquang@gmail.com",
+            "phone_number" => "0123456789",
+            "username" => "haquanq",
+            "role" => UserRole::ADMIN,
+            "password" => Hash::make("12345678"),
+            "created_at" => Carbon::now(),
+            "updated_at" => Carbon::now(),
+            "position_id" => 44,
+        ]);
     }
 }
