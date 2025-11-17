c73c0ea5b829f242f1d0214993f3a6efb6dc0ad1
Ha Quanq <quangha0120010@gmail.com>
2025-11-17 22:02:09 +0700

    Server: add array helper

---
+++ apps/server/app/Helpers/ArrayHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Log;
+
+class ArrayHelper
+{
+    public static function convertKeys(array $data, callable $converter): array
+    {
+        $newArray = [];
+        foreach ($data as $key => $value) {
+            $newValue = $value;
+
+            if (\is_array($value)) {
+                $newValue = static::convertKeys($value, $converter);
+            }
+
+            $newArray[$converter(\strval($key))] = $newValue;
+        }
+        Log::info(json_encode($newArray));
+        return $newArray;
+    }
+}
