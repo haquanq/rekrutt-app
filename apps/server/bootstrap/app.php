97f4e50dde09fc42b592aa6a7f76b9979579a829
Ha Quanq <quangha0120010@gmail.com>
2025-11-18 15:54:27 +0700

    Server: add exception handlers

--- apps/server/bootstrap/app.php
+++ apps/server/bootstrap/app.php
@@ -2,9 +2,12 @@
 
 use App\Middlewares\GlobalResponseWrapperMiddleware;
 use App\Middlewares\ProtectedRouteMiddleware;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(api: __DIR__ . "/../routes/api.php", commands: __DIR__ . "/../routes/console.php", health: "/up")
@@ -13,6 +16,23 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append(GlobalResponseWrapperMiddleware::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (Throwable $exception) {
+            if ($exception->getPrevious() instanceof AuthorizationException) {
+                return response()->json(
+                    [
+                        "message" => "Permission denied",
+                    ],
+                    Response::HTTP_FORBIDDEN,
+                );
+            }
+            if ($exception->getPrevious() instanceof ModelNotFoundException) {
+                return response()->json(
+                    [
+                        "message" => "Resource not found",
+                    ],
+                    Response::HTTP_NOT_FOUND,
+                );
+            }
+        });
     })
     ->create();
