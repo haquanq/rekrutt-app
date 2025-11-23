e419f6b3d916384d417958a5c2ac98a03cfb0a43
Ha Quanq <quangha0120010@gmail.com>
2025-11-24 03:18:33 +0700

    Server: use secure cookies for tokens

--- apps/server/app/Modules/Auth/Controllers/AuthController.php
+++ apps/server/app/Modules/Auth/Controllers/AuthController.php
@@ -3,12 +3,9 @@
 namespace App\Modules\Auth\Controllers;
 
 use App\Abstracts\BaseController;
-use App\Modules\Auth\Requests\ChangePasswordRequest;
 use App\Modules\Auth\Requests\LoginRequest;
 use App\Modules\Auth\Resources\UserResource;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Queue;
 
 class AuthController extends BaseController
 {
@@ -19,8 +16,8 @@ class AuthController extends BaseController
             return $this->unauthorizedResponse("Invalid credentials.");
         }
 
-        $cookie = cookie("jwt_token", $token, config("jwt.ttl"), sameSite: "none");
-        return $this->okResponse([new UserResource(Auth::user())])->withCookie($cookie);
+        $cookie = cookie("jwt_token", $token, config("jwt.ttl"), sameSite: "none", secure: true);
+        return $this->okResponse(new UserResource(Auth::user()))->withCookie($cookie);
     }
 
     public function logout()
@@ -31,8 +28,9 @@ class AuthController extends BaseController
 
     public function refresh()
     {
-        $cookie = cookie("jwt_token", Auth::refresh(), config("jwt.ttl"), sameSite: "none");
-        return $this->okResponse([new UserResource(Auth::user())])->withCookie($cookie);
+        $token = Auth::refresh();
+        $cookie = cookie("jwt_token", $token, config("jwt.ttl"), sameSite: "none", secure: true);
+        return $this->okResponse(new UserResource(Auth::user()))->withCookie($cookie);
     }
 
     public function me()
