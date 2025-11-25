5a3ad83d8b4330ca49c46845b8e5a4dd8b00a070
Ha Quanq <quangha0120010@gmail.com>
2025-11-26 00:29:45 +0700

    Server: add non-active user handler

--- apps/server/app/Middlewares/ProtectedRouteMiddleware.php
+++ apps/server/app/Middlewares/ProtectedRouteMiddleware.php
@@ -2,16 +2,24 @@
 
 namespace App\Middlewares;
 
+use App\Modules\Auth\Enums\UserStatus;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProtectedRouteMiddleware
 {
     public function handle(Request $request, Closure $next)
     {
         if (Auth::check() === false) {
-            return response()->json(["message" => "Unauthorized."], 401);
+            return response()->json(["message" => "Unauthorized."], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $status = UserStatus::tryFrom(Auth::user()->status);
+
+        if ($status !== UserStatus::ACTIVE) {
+            return response()->json(["message" => "Your account is no longer active."], Response::HTTP_FORBIDDEN);
         }
 
         return $next($request);
