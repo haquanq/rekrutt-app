2b7abd51849af4ed4856b612ad9badf6cc68bc8b
Ha Quanq <quangha0120010@gmail.com>
2025-11-26 01:15:21 +0700

    Server: add RatingScare routes

--- apps/server/routes/api.php
+++ apps/server/routes/api.php
@@ -6,6 +6,7 @@ use App\Modules\Department\Controllers\DepartmentController;
 use App\Modules\Position\Controllers\PositionController;
 use App\Modules\RatingScale\Controllers\RatingScaleController;
 use App\Modules\RatingScale\Controllers\RatingScalePointController;
+use App\Modules\RatingScale\Models\RatingScale;
 use Illuminate\Support\Facades\Route;
 
 Route::group(["prefix" => "auth", "middleware" => "protected"], function () {
@@ -47,6 +48,25 @@ Route::middleware("protected")->group(function () {
         ->prefix("positions")
         ->group(function () {
             Route::get("/", "index");
+            Route::get("/{id}", "show");
+            Route::post("/", "store");
+            Route::put("/{id}", "update");
+            Route::delete("/{id}", "destroy");
+        });
+
+    Route::controller(RatingScaleController::class)
+        ->prefix("rating-scales")
+        ->group(function () {
+            Route::get("/", "index");
+            Route::get("/{id}", "show");
+            Route::post("/", "store");
+            Route::put("/{id}", "update");
+            Route::delete("/{id}", "destroy");
+        });
+
+    Route::controller(RatingScalePointController::class)
+        ->prefix("rating-scale-points")
+        ->group(function () {
             Route::get("/{id}", "show");
             Route::post("/", "store");
             Route::put("/{id}", "update");
