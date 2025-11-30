e269f250a293b4786c251561e2d358a2d9a3efb1
Ha Quanq <quangha0120010@gmail.com>
2025-12-01 03:24:01 +0700

    Server: add custom Scramble transformers

---
+++ apps/server/app/Providers/ScrambleServiceProvider.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Providers;
+
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\Operation;
+use Dedoc\Scramble\Support\Generator\Reference;
+use Dedoc\Scramble\Support\Generator\Schema;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\ServiceProvider;
+use Str;
+
+class ScrambleServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        Scramble::configure()
+            ->withDocumentTransformers(function (OpenApi $document) {
+                $this->updateSchemas($document);
+            })
+            ->withOperationTransformers(function (Operation $operation) {
+                $this->updateOperationRequestBodies($operation);
+                $this->updateOperationResponses($operation);
+            });
+    }
+
+    public function updateSchemas(OpenApi $document)
+    {
+        $newSchemas = [];
+        foreach ($document->components->schemas as $schemaName => $schema) {
+            $newSchemaName = str_replace(["Request", "Resource"], "Dto", $schemaName);
+            $schema->setTitle($newSchemaName);
+
+            $newProperties = [];
+            if (isset($schema->type->properties)) {
+                foreach ($schema->type->properties as $propertyName => $propertyMetadata) {
+                    $newProperties[Str::camel($propertyName)] = $propertyMetadata;
+                }
+            }
+            $schema->type->properties = $newProperties;
+            $newSchemas[$newSchemaName] = $schema;
+        }
+        $document->components->schemas = $newSchemas;
+    }
+
+    public function updateOperationRequestBodies(Operation $operation)
+    {
+        if (!isset($operation->requestBodyObject)) {
+            return;
+        }
+
+        foreach ($operation->requestBodyObject->content as $contentType => $content) {
+            if ($content instanceof Reference) {
+                $newName = str_replace(["Request", "Resource"], "Dto", $content->fullName);
+                $content->fullName = $newName;
+            }
+        }
+    }
+
+    public function updateOperationResponses(Operation $operation)
+    {
+        if (!isset($operation->responses)) {
+            return;
+        }
+
+        foreach ($operation->responses as $response) {
+            if (!isset($response->content)) {
+                continue;
+            }
+
+            foreach ($response->content as $contentType => $content) {
+                if ($content instanceof Schema) {
+                    if ($content->type instanceof Reference) {
+                        $newName = str_replace(["Request", "Resource"], "Dto", $content->type->fullName);
+                        $content->type->fullName = $newName;
+                        $content->setTitle($newName);
+                    } elseif (isset($content->type->items)) {
+                        $newName = str_replace(["Request", "Resource"], "Dto", $content->type->items->fullName);
+                        $content->type->items->fullName = $newName;
+                        $content->setTitle($newName);
+                    }
+                }
+            }
+        }
+    }
+}
