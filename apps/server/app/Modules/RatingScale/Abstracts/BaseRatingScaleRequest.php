91b50e70b035bb0f7beb268cabe0e321dc0adede
Ha Quanq <quangha0120010@gmail.com>
2025-12-02 00:24:53 +0700

    Server: add base RatingScale validations docs

--- apps/server/app/Modules/RatingScale/Abstracts/BaseRatingScaleRequest.php
+++ apps/server/app/Modules/RatingScale/Abstracts/BaseRatingScaleRequest.php
@@ -14,8 +14,20 @@ abstract class BaseRatingScaleRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
+            /**
+             * Name
+             * @example 10-Point Numerical Scale
+             */
             "name" => ["required", "string", "max:100"],
+            /**
+             * Description
+             * @example The 10-Point Numerical Scale is a rating scale that ranges from 0 to 10, with 0 being the lowest rating and 10 being the highest rating.
+             */
             "description" => ["string", "max:500"],
+            /**
+             * Whether the rating scale is active
+             * @example true
+             */
             "is_active" => ["required", "boolean"],
         ];
     }
