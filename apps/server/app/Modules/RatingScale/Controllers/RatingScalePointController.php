67431c6c204a03050d1af7fd77ce4e75f9083d16
Ha Quanq <quangha0120010@gmail.com>
2025-12-02 00:39:15 +0700

    Server: add RatingScalePoint endpoints docs

--- apps/server/app/Modules/RatingScale/Controllers/RatingScalePointController.php
+++ apps/server/app/Modules/RatingScale/Controllers/RatingScalePointController.php
@@ -8,12 +8,52 @@ use App\Modules\RatingScale\Requests\RatingScalePointUpdateRequest;
 use App\Modules\RatingScale\Models\RatingScalePoint;
 use App\Modules\RatingScale\Resources\RatingScalePointResource;
 use App\Modules\RatingScale\Resources\RatingScalePointResourceCollection;
+use Dedoc\Scramble\Attributes\QueryParameter;
 use Illuminate\Support\Facades\Gate;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class RatingScalePointController extends BaseController
 {
+    /**
+     * Find all rating scale points
+     *
+     * Return a list of rating scale points. Allows pagination, relations and filter query.
+     */
+    #[
+        QueryParameter(
+            name: "page[number]",
+            type: "integer",
+            description: "Current page number (default: 1)",
+            example: 1,
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "page[size]",
+            type: "integer",
+            description: "Size of current page (default: 15, max: 100)",
+            example: 15,
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "include",
+            type: "string",
+            description: "Include nested relations </br>" .
+                " Allow relations: ratingScale </br>" .
+                "Example: include=ratingScale",
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "filter[*]",
+            type: "string",
+            description: "Filter by fields </br>" .
+                "Allow fields: ratingScaleId </br>" .
+                "Example: filter[ratingScaleId]=1",
+        ),
+    ]
     public function index()
     {
         Gate::authorize("viewAny", RatingScalePoint::class);
@@ -26,6 +66,20 @@ class RatingScalePointController extends BaseController
         return RatingScalePointResourceCollection::make($ratingScalePoints);
     }
 
+    /**
+     * Find rating scale point by Id
+     *
+     * Return a unique rating scale point. Allow relations query
+     */
+    #[
+        QueryParameter(
+            name: "include",
+            type: "string",
+            description: "Include nested relations </br>" .
+                " Allow relations: ratingScale </br>" .
+                "Example: include=ratingScale",
+        ),
+    ]
     public function show(int $id)
     {
         Gate::authorize("view", RatingScalePoint::class);
@@ -37,6 +91,11 @@ class RatingScalePointController extends BaseController
         return RatingScalePointResource::make($ratingScalePoint);
     }
 
+    /**
+     * Create rating scale point
+     *
+     * Return created rating scale point
+     */
     public function store(RatingScalePointStoreRequest $request)
     {
         Gate::authorize("create", RatingScalePoint::class);
@@ -44,6 +103,11 @@ class RatingScalePointController extends BaseController
         return $this->createdResponse(new RatingScalePointResource($createdRatingScalePoint));
     }
 
+    /**
+     * Update rating scale point
+     *
+     *  Return no content
+     */
     public function update(RatingScalePointUpdateRequest $request, int $id)
     {
         Gate::authorize("update", RatingScalePoint::class);
@@ -51,6 +115,11 @@ class RatingScalePointController extends BaseController
         return $this->noContentResponse();
     }
 
+    /**
+     * Delete rating scale point by Id
+     *
+     * Permanently delete rating scale point. Return no content
+     */
     public function destroy(int $id)
     {
         Gate::authorize("delete", RatingScalePoint::class);
