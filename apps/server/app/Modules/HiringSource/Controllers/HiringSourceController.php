8a65606e970c18c57d9714d3b409a57143644d4b
Ha Quanq <quangha0120010@gmail.com>
2025-12-02 00:10:01 +0700

    Server: add HiringSource pagination query

--- apps/server/app/Modules/HiringSource/Controllers/HiringSourceController.php
+++ apps/server/app/Modules/HiringSource/Controllers/HiringSourceController.php
@@ -7,6 +7,7 @@ use App\Modules\HiringSource\Requests\HiringSourceStoreRequest;
 use App\Modules\HiringSource\Requests\HiringSourceUpdateRequest;
 use App\Modules\HiringSource\Models\HiringSource;
 use App\Modules\HiringSource\Resources\HiringSourceResource;
+use App\Modules\HiringSource\Resources\HiringSourceResourceCollection;
 use Illuminate\Support\Facades\Gate;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
@@ -19,16 +20,16 @@ class HiringSourceController extends BaseController
 
         $hiringSources = QueryBuilder::for(HiringSource::class)
             ->allowedFilters([AllowedFilter::partial("name")])
-            ->get();
+            ->autoPaginate();
 
-        return $this->okResponse(HiringSourceResource::collection($hiringSources));
+        return HiringSourceResourceCollection::make($hiringSources);
     }
 
     public function show(int $id)
     {
         Gate::authorize("view", HiringSource::class);
         $hiringSource = HiringSource::findOrFail($id);
-        return $this->okResponse(new HiringSourceResource($hiringSource));
+        return HiringSourceResource::make($hiringSource);
     }
 
     public function store(HiringSourceStoreRequest $request)
