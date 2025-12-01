1e34041167dcbd4bd56f7adee6142c89e6d9befc
Ha Quanq <quangha0120010@gmail.com>
2025-12-02 00:09:11 +0700

    Server: add HiringSourceResourceCollection

---
+++ apps/server/app/Modules/HiringSource/Resources/HiringSourceResourceCollection.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\HiringSource\Resources;
+
+use App\Abstracts\BaseResourceCollection;
+
+class HiringSourceResourceCollection extends BaseResourceCollection
+{
+    public $collects = HiringSourceResource::class;
+}
