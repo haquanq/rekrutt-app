5aa93ff250d5d38bf2b7b328466081c3e41db69a
Ha Quanq <quangha0120010@gmail.com>
2025-12-02 00:13:40 +0700

    Serve: add HiringSource endpoints docs

--- apps/server/app/Modules/HiringSource/Controllers/HiringSourceController.php
+++ apps/server/app/Modules/HiringSource/Controllers/HiringSourceController.php
@@ -8,12 +8,41 @@ use App\Modules\HiringSource\Requests\HiringSourceUpdateRequest;
 use App\Modules\HiringSource\Models\HiringSource;
 use App\Modules\HiringSource\Resources\HiringSourceResource;
 use App\Modules\HiringSource\Resources\HiringSourceResourceCollection;
+use Dedoc\Scramble\Attributes\QueryParameter;
 use Illuminate\Support\Facades\Gate;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class HiringSourceController extends BaseController
 {
+    /**
+     * Find all hiring sources
+     *
+     * Return a list of hiring sources. Allows pagination and filter query.
+     */
+    #[
+        QueryParameter(
+            name: "page[number]",
+            type: "integer",
+            description: "Current page number (default: 1)",
+            example: 1,
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "page[size]",
+            type: "integer",
+            description: "Size of current page (default: 15, max: 100)",
+            example: 15,
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "filter[*]",
+            type: "string",
+            description: "Filter by fields </br>" . "Allow fields: name </br>" . "Example: filter[name]=LinkedIn",
+        ),
+    ]
     public function index()
     {
         Gate::authorize("viewAny", HiringSource::class);
@@ -25,6 +54,11 @@ class HiringSourceController extends BaseController
         return HiringSourceResourceCollection::make($hiringSources);
     }
 
+    /**
+     * Find hiring source by Id
+     *
+     * Return a unique hiring source
+     */
     public function show(int $id)
     {
         Gate::authorize("view", HiringSource::class);
@@ -32,6 +66,11 @@ class HiringSourceController extends BaseController
         return HiringSourceResource::make($hiringSource);
     }
 
+    /**
+     * Create hiring source
+     *
+     * Return created hiring source
+     */
     public function store(HiringSourceStoreRequest $request)
     {
         Gate::authorize("create", HiringSource::class);
@@ -39,6 +78,11 @@ class HiringSourceController extends BaseController
         return $this->createdResponse(new HiringSourceResource($createdHiringSource));
     }
 
+    /**
+     * Update hiring source
+     *
+     * Return no content
+     */
     public function update(HiringSourceUpdateRequest $request, int $id)
     {
         Gate::authorize("update", HiringSource::class);
@@ -46,6 +90,11 @@ class HiringSourceController extends BaseController
         return $this->noContentResponse();
     }
 
+    /**
+     * Delete hiring source by Id
+     *
+     * Permanently delete hiring source. Return no content
+     */
     public function destroy(int $id)
     {
         Gate::authorize("delete", HiringSource::class);
