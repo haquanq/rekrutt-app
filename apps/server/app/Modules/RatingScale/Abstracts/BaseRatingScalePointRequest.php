3af1257a03cad40cc601b4315dd358c9fd6b3f69
Ha Quanq <quangha0120010@gmail.com>
2025-12-02 00:28:46 +0700

    Server: add base RatingScalePoint validations docs

--- apps/server/app/Modules/RatingScale/Abstracts/BaseRatingScalePointRequest.php
+++ apps/server/app/Modules/RatingScale/Abstracts/BaseRatingScalePointRequest.php
@@ -14,9 +14,25 @@ abstract class BaseRatingScalePointRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
+            /**
+             * Rank/order
+             * @example 1
+             */
             "rank" => ["required", "integer"],
+            /**
+             * Label
+             * @example 9 - Pretty good
+             */
             "label" => ["required", "string", "max:100"],
+            /**
+             * Description
+             * @example Candidate scored 9 out of 10
+             */
             "definition" => ["required", "string", "max:300"],
+            /**
+             * Rating scale Id of which this rating scale point belongs
+             * @example 1
+             */
             "rating_scale_id" => ["required", "integer", "exists:rating_scale,id"],
         ];
     }
