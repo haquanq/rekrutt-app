c6ec5543e60c58014c0ba688056162b822f33701
Ha Quanq <quangha0120010@gmail.com>
2025-12-03 05:07:14 +0700

    Server: register global Route pattern for {id}

--- apps/server/app/Providers/AppServiceProvider.php
+++ apps/server/app/Providers/AppServiceProvider.php
@@ -3,12 +3,15 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        Route::pattern("id", "[0-9]+");
+
         Builder::macro("autoPaginate", function (
             $defaultPerPage = 15,
             $columns = ["*"],
