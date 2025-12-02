b7ac520f9585b7fb64c3c996518633d466d6f020
Ha Quanq <quangha0120010@gmail.com>
2025-12-03 04:32:00 +0700

    Server: add ProposalController docs

--- apps/server/app/Modules/Proposal/Controllers/ProposalController.php
+++ apps/server/app/Modules/Proposal/Controllers/ProposalController.php
@@ -9,12 +9,55 @@ use App\Modules\Proposal\Requests\ProposalUpdateRequest;
 use App\Modules\Proposal\Resources\ProposalResource;
 use App\Modules\Proposal\Models\Proposal;
 use App\Modules\Proposal\Resources\ProposalResourceCollection;
+use Dedoc\Scramble\Attributes\QueryParameter;
 use Illuminate\Support\Facades\Gate;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class ProposalController extends BaseController
 {
+    /**
+     * Find all proposals
+     *
+     * Return a list of proposals. Allows pagination, relations and filter query.
+     *
+     * Authorization rules:
+     * - User with roles: any.
+     */
+    #[
+        QueryParameter(
+            name: "page[number]",
+            type: "integer",
+            description: "Current page number (default: 1)",
+            example: 1,
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "page[size]",
+            type: "integer",
+            description: "Size of current page (default: 15, max: 100)",
+            example: 15,
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "include",
+            type: "string",
+            description: "Include nested relations </br>" .
+                " Allow relations: position, contractType, educationLevel, experienceLevel, createdBy, reviewedBy </br>" .
+                "Example: include=position,createdBy",
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "filter[*]",
+            type: "string",
+            description: "Filter by fields </br>" .
+                "Allow fields: status, title, createdByUserId, reviewedByUserId, positionId </br>" .
+                "Example: filter[status]=APPROVED",
+        ),
+    ]
     public function index()
     {
         Gate::authorize("viewAny", Proposal::class);
@@ -40,6 +83,23 @@ class ProposalController extends BaseController
         return ProposalResourceCollection::make($proposals);
     }
 
+    /**
+     * Find proposal by Id
+     *
+     * Return a unique proposal. Allows relations query.
+     *
+     * Authorization rules:
+     * - User with roles: any.
+     */
+    #[
+        QueryParameter(
+            name: "include",
+            type: "string",
+            description: "Include nested relations </br>" .
+                " Allow relations: position, contractType, educationLevel, experienceLevel, createdBy, reviewedBy </br>" .
+                "Example: include=position,createdBy",
+        ),
+    ]
     public function show(int $id)
     {
         Gate::authorize("view", Proposal::class);
@@ -58,18 +118,44 @@ class ProposalController extends BaseController
         return ProposalResource::make($proposal);
     }
 
+    /**
+     * Create proposal
+     *
+     * Return a unique proposal
+     *
+     * Authorization rules:
+     * - User with roles: MANAGER, HIRING_MANAGER.
+     */
     public function store(ProposalStoreRequest $request)
     {
         $createdProposal = Proposal::create($request->validated());
         return $this->createdResponse(new ProposalResource($createdProposal));
     }
 
+    /**
+     * Update proposal
+     *
+     * Return no content
+     *
+     * Authorization rules:
+     * - User with roles: MANAGER, HIRING_MANAGER.
+     * - User must be the author of the proposal.
+     */
     public function update(ProposalUpdateRequest $request)
     {
         $request->proposal->update($request->validated());
         return $this->noContentResponse();
     }
 
+    /**
+     * Detele proposal
+     *
+     * Permanently delete proposal. Return no content
+     *
+     * Authorization rules:
+     * - User with roles: MANAGER, HIRING_MANAGER.
+     * - User must be the author of the proposal.
+     */
     public function destroy(int $id)
     {
         $proposal = Proposal::findOrFail($id);
