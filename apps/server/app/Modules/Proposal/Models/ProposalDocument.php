4f66d2994b171409479e7d3974ce019dd45ef88f
Ha Quanq <quangha0120010@gmail.com>
2025-12-03 06:12:22 +0700

    Server: update ProposalDocument guarded fields

--- apps/server/app/Modules/Proposal/Models/ProposalDocument.php
+++ apps/server/app/Modules/Proposal/Models/ProposalDocument.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProposalDocument extends BaseModel
 {
+    protected $guarded = ["id", "created_at", "updated_at"];
     public function proposal(): BelongsTo
     {
         return $this->belongsTo(Proposal::class);
