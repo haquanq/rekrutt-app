bcbc8b525dc8599577c85ec5144ac5e1fed2eb41
Ha Quanq <quangha0120010@gmail.com>
2025-12-04 06:32:18 +0700

    Server: remove loggers from ProposalPolicy

--- apps/server/app/Modules/Proposal/Policies/ProposalPolicy.php
+++ apps/server/app/Modules/Proposal/Policies/ProposalPolicy.php
@@ -6,7 +6,6 @@ use App\Modules\Auth\Enums\UserRole;
 use App\Modules\Auth\Models\User;
 use App\Modules\Proposal\Models\Proposal;
 use Illuminate\Auth\Access\Response;
-use Illuminate\Support\Facades\Log;
 
 class ProposalPolicy
 {
@@ -31,7 +30,6 @@ class ProposalPolicy
 
     public function update(User $user, Proposal $proposal): Response
     {
-        Log::info(json_encode($user->department));
         if (!$user->hasRole(UserRole::MANAGER, UserRole::HIRING_MANAGER)) {
             return Response::deny("You are not allowed to update this proposal");
         } elseif ($user->id !== $proposal->created_by_user_id) {
