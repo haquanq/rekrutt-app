64c6dc1031c8a8fe335e3128307a493adde80a39
Ha Quanq <quangha0120010@gmail.com>
2025-12-04 06:35:30 +0700

    Server: update CandidatePolicy

--- apps/server/app/Modules/Candidate/Policies/CandidatePolicy.php
+++ apps/server/app/Modules/Candidate/Policies/CandidatePolicy.php
@@ -4,6 +4,7 @@ namespace App\Modules\Candidate\Policies;
 
 use App\Modules\Auth\Enums\UserRole;
 use App\Modules\Auth\Models\User;
+use Illuminate\Auth\Access\Response;
 
 class CandidatePolicy
 {
@@ -17,21 +18,30 @@ class CandidatePolicy
         return true;
     }
 
-    public function create(User $user): bool
+    public function create(User $user): Response
     {
-        $role = UserRole::tryFrom($user["role"]);
-        return \in_array($role, [UserRole::HIRING_MANAGER, UserRole::RECRUITER]);
+        if (!$user->hasRole(UserRole::RECRUITER, UserRole::HIRING_MANAGER)) {
+            return Response::deny("You are not allowed to create new candidate.");
+        }
+
+        return Response::allow();
     }
 
-    public function update(User $user): bool
+    public function update(User $user): Response
     {
-        $role = UserRole::tryFrom($user["role"]);
-        return \in_array($role, [UserRole::HIRING_MANAGER, UserRole::RECRUITER]);
+        if (!$user->hasRole(UserRole::RECRUITER, UserRole::HIRING_MANAGER)) {
+            return Response::deny("You are not allowed to update this candidate.");
+        }
+
+        return Response::allow();
     }
 
-    public function delete(User $user): bool
+    public function delete(User $user): Response
     {
-        $role = UserRole::tryFrom($user["role"]);
-        return \in_array($role, [UserRole::HIRING_MANAGER, UserRole::RECRUITER]);
+        if (!$user->hasRole(UserRole::RECRUITER, UserRole::HIRING_MANAGER)) {
+            return Response::deny("You are not allowed to delete this candidate.");
+        }
+
+        return Response::allow();
     }
 }
