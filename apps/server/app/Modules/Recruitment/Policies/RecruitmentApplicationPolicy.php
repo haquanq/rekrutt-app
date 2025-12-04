e75b1e6ae06381936a60e4eebb5e97fb75684d9c
Ha Quanq <quangha0120010@gmail.com>
2025-12-05 06:39:09 +0700

    Server: add RecruitmentApplicationPolicy

---
+++ apps/server/app/Modules/Recruitment/Policies/RecruitmentApplicationPolicy.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Modules\Recruitment\Policies;
+
+use App\Modules\Auth\Enums\UserRole;
+use App\Modules\Auth\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class RecruitmentApplicationPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): Response
+    {
+        if (!$user->hasRole(UserRole::HIRING_MANAGER, UserRole::RECRUITER)) {
+            return Response::deny("You are not allowed to create new recruitment application.");
+        }
+
+        return Response::allow();
+    }
+
+    public function updatePriority(User $user): Response
+    {
+        if (!$user->hasRole(UserRole::HIRING_MANAGER)) {
+            return Response::deny("You are not allowed to update any recruitment application's priority.");
+        }
+
+        return Response::allow();
+    }
+
+    public function delete(User $user): Response
+    {
+        if (!$user->hasRole(UserRole::HIRING_MANAGER, UserRole::RECRUITER)) {
+            return Response::deny("You are not allowed to delete any recruitment application.");
+        }
+
+        return Response::allow();
+    }
+}
