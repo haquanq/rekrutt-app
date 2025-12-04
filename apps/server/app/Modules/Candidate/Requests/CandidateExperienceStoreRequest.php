e8da75b6c2556cb8e7f7fec6839ce5c6169ea2a7
Ha Quanq <quangha0120010@gmail.com>
2025-12-05 01:00:53 +0700

    Server: add CandidateExperienceStoreRequest validation rule

--- apps/server/app/Modules/Candidate/Requests/CandidateExperienceStoreRequest.php
+++ apps/server/app/Modules/Candidate/Requests/CandidateExperienceStoreRequest.php
@@ -3,11 +3,28 @@
 namespace App\Modules\Candidate\Requests;
 
 use App\Modules\Candidate\Abstracts\BaseCandidateExperienceRequest;
+use App\Modules\Candidate\Enums\CandidateStatus;
 use App\Modules\Candidate\Models\CandidateExperience;
+use App\Modules\Candidate\Rules\CandidateExistsWithStatusRule;
 use Illuminate\Support\Facades\Gate;
 
 class CandidateExperienceStoreRequest extends BaseCandidateExperienceRequest
 {
+    public function rules(): array
+    {
+        return parent::rules() + [
+            /**
+             * Id of Candidate
+             * @example 1
+             */
+            "candidate_id" => [
+                "required",
+                "integer:strict",
+                new CandidateExistsWithStatusRule(CandidateStatus::PENDING),
+            ],
+        ];
+    }
+
     public function authorize(): bool
     {
         Gate::authorize("create", CandidateExperience::class);
