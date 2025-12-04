94ab640d3c2fade7b91b5bb49b72122cd004c6c6
Ha Quanq <quangha0120010@gmail.com>
2025-12-04 17:16:57 +0700

    Server: update base CandidateExperience validation rules docs

--- apps/server/app/Modules/Candidate/Abstracts/BaseCandidateExperienceRequest.php
+++ apps/server/app/Modules/Candidate/Abstracts/BaseCandidateExperienceRequest.php
@@ -14,13 +14,45 @@ abstract class BaseCandidateExperienceRequest extends BaseFormRequest
     public function rules(): array
     {
         return [
+            /**
+             * From date
+             * @example 2020-01-01
+             */
             "from_date" => ["required", "date", "before:today"],
+            /**
+             * To date
+             * @example 2022-01-01
+             */
             "to_date" => ["required", "date", "before:today"],
+            /**
+             * Emplpoyer name
+             * @example KFC Tech
+             */
             "employer_name" => ["required", "string", "max:100"],
+            /**
+             * Emplpoyer description
+             * @example Provide ERP solutions
+             */
             "employer_description" => ["string", "max:500"],
+            /**
+             * Position title
+             * @example Software Engineer
+             */
             "position_title" => ["required", "string", "max:100"],
+            /**
+             * Position duty
+             * @example Work with PHP and Laravel
+             */
             "position_duty" => ["required", "string", "max:500"],
+            /**
+             * Note
+             * @example Need further background check
+             */
             "note" => ["string", "max:500"],
+            /**
+             * Id of Candidate
+             * @example 1
+             */
             "candidate_id" => ["required", "integer:strict", "exists:candidate,id"],
         ];
     }
