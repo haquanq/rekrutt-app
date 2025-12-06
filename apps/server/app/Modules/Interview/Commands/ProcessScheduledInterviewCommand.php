3f8cca434e83d617c0e77eb31a7edab2c03fcf2c
Ha Quanq <quangha0120010@gmail.com>
2025-12-06 17:59:38 +0700

    Server: add command to process scheduled interviews

---
+++ apps/server/app/Modules/Interview/Commands/ProcessScheduledInterviewCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Modules\Recruitment\Commands;
+
+use App\Modules\Interview\Enums\InterviewStatus;
+use App\Modules\Interview\Models\Interview;
+use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class ProcessScheduledInterviewCommand extends Command
+{
+    protected $signature = "interview:process-sheduled";
+    protected $description = "Process scheduled interview statuses.";
+
+    public function handle(): void
+    {
+        $currentTimeText = Carbon::now()->toDateTimeString();
+        $completedStatus = InterviewStatus::COMPLETED->value;
+        $inProgressStatus = InterviewStatus::IN_PROGRESS->value;
+
+        Interview::query()->update([
+            "status" => DB::raw(
+                "CASE WHEN started_at <= '$currentTimeText' THEN '$inProgressStatus' " .
+                    " WHEN ended_at <= '$currentTimeText' THEN '$completedStatus' ",
+            ),
+        ]);
+    }
+}
--- apps/server/routes/console.php
+++ apps/server/routes/console.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command("recruitment:process-sheduled")->everyMinute();
+Schedule::command("interview:process-sheduled")->everyMinute();
