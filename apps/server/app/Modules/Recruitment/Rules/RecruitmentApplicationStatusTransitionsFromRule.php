6fe843b9618f3193bdcce04606622f81ba9fe18a
Ha Quanq <quangha0120010@gmail.com>
2025-12-08 00:05:04 +0700

    Server: add RecruitmentApplicationStatusTransitionsFromRule

---
+++ apps/server/app/Modules/Recruitment/Rules/RecruitmentApplicationStatusTransitionsFromRule.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Modules\Recruitment\Rules;
+
+use App\Modules\Recruitment\Enums\RecruitmentApplicationStatus;
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class RecruitmentApplicationStatusTransitionsFromRule implements ValidationRule
+{
+    public function __construct(protected RecruitmentApplicationStatus $oldStatus) {}
+
+    public function validate(string $attribute, mixed $newStatus, Closure $fail): void
+    {
+        $newStatus = RecruitmentApplicationStatus::tryFrom($newStatus);
+
+        if (!$newStatus) {
+            $fail("The selected recruitment application status is invalid.");
+            return;
+        }
+
+        $transitions = [
+            RecruitmentApplicationStatus::PENDING->value => [
+                RecruitmentApplicationStatus::INTERVIEWING,
+                RecruitmentApplicationStatus::DISCARDED,
+            ],
+            RecruitmentApplicationStatus::INTERVIEWING->value => [
+                RecruitmentApplicationStatus::OFFER_PENDING,
+                RecruitmentApplicationStatus::WITHDRAWN,
+                RecruitmentApplicationStatus::DISCARDED,
+            ],
+            RecruitmentApplicationStatus::OFFER_PENDING->value => [
+                RecruitmentApplicationStatus::OFFER_ACCEPTED,
+                RecruitmentApplicationStatus::OFFER_REJECTED,
+            ],
+            RecruitmentApplicationStatus::OFFER_ACCEPTED->value => [],
+            RecruitmentApplicationStatus::OFFER_REJECTED->value => [],
+            RecruitmentApplicationStatus::WITHDRAWN->value => [],
+            RecruitmentApplicationStatus::DISCARDED->value => [],
+        ];
+
+        if ($this->oldStatus === $newStatus) {
+            return;
+        }
+
+        if (!\in_array($newStatus, $transitions[$this->oldStatus->value])) {
+            $fail("Can't change recruitment application status from {$this->oldStatus->value} to {$newStatus->value}.");
+            return;
+        }
+    }
+}
