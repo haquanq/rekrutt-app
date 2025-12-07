488b2d52edeab7a828f775ac4ebb724a528d49b7
Ha Quanq <quangha0120010@gmail.com>
2025-12-07 22:35:27 +0700

    Server: add InterviewEvaluationController

---
+++ apps/server/app/Modules/Interview/Controllers/InterviewEvaluationController.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace App\Modules\Interview\Controllers;
+
+use App\Abstracts\BaseController;
+use App\Modules\Interview\Enums\InterviewStatus;
+use App\Modules\Interview\Models\InterviewEvaluation;
+use App\Modules\Interview\Requests\InterviewEvaluationDestroyRequest;
+use App\Modules\Interview\Requests\InterviewEvaluationStoreRequest;
+use App\Modules\Interview\Requests\InterviewEvaluationUpdateRequest;
+use App\Modules\Interview\Resources\InterviewEvaluationResource;
+use App\Modules\Interview\Resources\InterviewEvaluationResourceCollection;
+use Dedoc\Scramble\Attributes\QueryParameter;
+use Illuminate\Support\Facades\Gate;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
+
+class InterviewEvaluationController extends BaseController
+{
+    /**
+     * Find all interview evaluations
+     *
+     * Return a list of interview evaluations. Allows pagination, relations and filters query.
+     *
+     * Authorization
+     * - User with roles: any.
+     */
+    #[
+        QueryParameter(
+            name: "page[number]",
+            type: "integer",
+            description: "Current page number (default: 1)",
+            example: 1,
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "page[size]",
+            type: "integer",
+            description: "Size of current page (default: 15, max: 100)",
+            example: 15,
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "include",
+            type: "string",
+            description: "Include nested relations </br>" .
+                " Allow relations: interview, ratingScalePoint, createdBy </br>" .
+                "Example: include=interview,createdBy,ratingScalePoint",
+        ),
+    ]
+    #[
+        QueryParameter(
+            name: "filter[*]",
+            type: "string",
+            description: "Filter by fields </br>" .
+                "Allow fields: createdByUserId, interviewId </br>" .
+                "Example: filter[interviewId]=14",
+        ),
+    ]
+    public function index()
+    {
+        Gate::authorize("viewAny", InterviewEvaluation::class);
+
+        $interviewMethods = QueryBuilder::for(InterviewEvaluation::class)
+            ->allowedIncludes(["interviews", "ratingScalePoint", "createdBy"])
+            ->allowedFilters([
+                AllowedFilter::exact("createdByUserId", "created_by_user_id"),
+                AllowedFilter::exact("interviewId", "interview_id"),
+            ])
+            ->autoPaginate();
+
+        return InterviewEvaluationResourceCollection::make($interviewMethods);
+    }
+
+    /**
+     * Find interview evaluation by Id
+     *
+     * Return a unique interview evaluation. Allow relations query.
+     *
+     * Authorization
+     * - User with roles: any.
+     */
+    #[
+        QueryParameter(
+            name: "include",
+            type: "string",
+            description: "Include nested relations </br>" .
+                " Allow relations: interview, ratingScalePoint, createdBy </br>" .
+                "Example: include=interview,createdBy,ratingScalePoint",
+        ),
+    ]
+    public function show(int $id)
+    {
+        Gate::authorize("view", InterviewEvaluation::class);
+
+        $interviewEvaluation = QueryBuilder::for(InterviewEvaluation::class)
+            ->allowedIncludes(["interviews", "ratingScalePoint", "createdBy"])
+            ->findOrFail($id);
+
+        return InterviewEvaluationResource::make($interviewEvaluation);
+    }
+
+    /**
+     * Create interview evaluation
+     *
+     * Return created interview evaluation.
+     *
+     * Authorization
+     * - User with roles: any
+     * - User must be participating in the interview
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function store(InterviewEvaluationStoreRequest $request)
+    {
+        $interviewStatus = $request->interview->status;
+
+        if ($interviewStatus !== InterviewStatus::UNDER_EVALUATION) {
+            throw new ConflictHttpException("Cannot create. " . $interviewStatus->description());
+        }
+
+        $createdInterviewEvaluation = InterviewEvaluation::create($request->validated());
+        return $this->createdResponse(InterviewEvaluationResource::make($createdInterviewEvaluation));
+    }
+
+    /**
+     * Update interview evaluation
+     *
+     * Return no content.
+     *
+     * Authorization
+     * - User with roles: any
+     * - User must be the creator of the interview evaluation
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function update(InterviewEvaluationUpdateRequest $request)
+    {
+        $interviewStatus = $request->interviewEvaluation->interview->status;
+
+        if ($interviewStatus !== InterviewStatus::UNDER_EVALUATION) {
+            throw new ConflictHttpException("Cannot update. " . $interviewStatus->description());
+        }
+
+        $request->interviewEvaluation->update($request->validated());
+        return $this->noContentResponse();
+    }
+
+    /**
+     * Delete interview evaluation by Id
+     *
+     * Permanently delete interview evaluation. Return no content.
+     *
+     * Authorization
+     * - User with roles: any
+     * - User must be the creator of the interview evaluation
+     *
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function destroy(InterviewEvaluationDestroyRequest $request)
+    {
+        $interviewStatus = $request->interviewEvaluation->interview->status;
+
+        if ($interviewStatus !== InterviewStatus::UNDER_EVALUATION) {
+            throw new ConflictHttpException("Cannot delete. " . $interviewStatus->description());
+        }
+
+        $request->interviewEvaluation->delete();
+        return $this->noContentResponse();
+    }
+}
