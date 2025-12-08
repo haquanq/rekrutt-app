e6a0f7439bd2e368a8fbf08548d48b4c5bb6bb6e
Ha Quanq <quangha0120010@gmail.com>
2025-12-08 07:51:04 +0700

    Server: update RecruitmentApplicationResource

--- apps/server/app/Modules/Recruitment/Resources/RecruitmentApplicationResource.php
+++ apps/server/app/Modules/Recruitment/Resources/RecruitmentApplicationResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Modules\Recruitment\Resources;
 
+use App\Modules\Auth\Resources\UserResource;
 use App\Modules\Candidate\Resources\CandidateResource;
 use App\Modules\Interview\Resources\InterviewResource;
 use Illuminate\Http\Request;
@@ -13,9 +14,17 @@ class RecruitmentApplicationResource extends JsonResource
     {
         return [
             "id" => $this->id,
-            "completed_at" => $this->completed_at,
             "status" => $this->status,
             "priority" => $this->priority,
+            "offer_started_at" => $this->offer_started_at,
+            "offer_expired_at" => $this->offer_expired_at,
+            "offer_responded_at" => $this->offer_responded_at,
+            "offer_rejected_reason" => $this->offer_rejected_reason,
+            "discarded_at" => $this->discarded_at,
+            "discarded_reason" => $this->discarded_reason,
+            "discarded_by" => UserResource::make($this->whenLoaded("discardedBy")),
+            "withdrawn_at" => $this->withdrawn_at,
+            "withdrawn_reason" => $this->withdrawn_reason,
             "created_at" => $this->created_at,
             "updated_at" => $this->updated_at,
             "recruitment" => RecruitmentResource::make($this->whenLoaded("recruitment")),
