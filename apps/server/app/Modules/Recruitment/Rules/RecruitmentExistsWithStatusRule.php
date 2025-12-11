e0dbc8912ce0583b135aa709b6783e55ed0e9c54
Ha Quanq <quangha0120010@gmail.com>
2025-12-11 11:50:16 +0700

    Server: refactor Recruitment rule

--- apps/server/app/Modules/Recruitment/Rules/RecruitmentExistsWithStatusRule.php
+++ apps/server/app/Modules/Recruitment/Rules/RecruitmentExistsWithStatusRule.php
@@ -9,37 +9,16 @@ use Illuminate\Contracts\Validation\ValidationRule;
 
 class RecruitmentExistsWithStatusRule implements ValidationRule
 {
-    private ?Recruitment $recruitment = null;
-    private bool $withRecruitment = false;
-
     public function __construct(protected RecruitmentStatus $requiredStatus) {}
-
-    public static function create(RecruitmentStatus $requiredStatus): self
-    {
-        return new self($requiredStatus);
-    }
-
-    public function withRecruitment(?Recruitment $recruitment): self
-    {
-        $this->recruitment = $recruitment;
-        $this->withRecruitment = true;
-        return $this;
-    }
 
     public function validate(string $attribute, mixed $id, Closure $fail): void
     {
-        if (!$this->recruitment && !$this->withRecruitment) {
-            $this->recruitment = Recruitment::find($id);
-        }
+        $recruitment = Recruitment::find($id, ["status"]);
 
-        if (!$this->recruitment) {
+        if ($recruitment === null) {
             $fail("Recruitment does not exist.");
-            return;
-        }
-
-        if ($this->recruitment->status !== $this->requiredStatus) {
+        } elseif ($recruitment->status !== $this->requiredStatus) {
             $fail("Recruitment must have status of {$this->requiredStatus->value}.");
-            return;
         }
     }
 }
