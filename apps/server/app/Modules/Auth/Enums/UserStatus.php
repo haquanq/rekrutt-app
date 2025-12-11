0f588b50bc7d9fecacc7b94ef793906578ca0cfb
Ha Quanq <quangha0120010@gmail.com>
2025-12-12 00:46:14 +0700

    Server: add UserStatus descriptions

--- apps/server/app/Modules/Auth/Enums/UserStatus.php
+++ apps/server/app/Modules/Auth/Enums/UserStatus.php
@@ -7,4 +7,13 @@ enum UserStatus: string
     case ACTIVE = "ACTIVE";
     case SUSPENDED = "SUSPENDED";
     case RETIRED = "RETIRED";
+
+    public function description(): string
+    {
+        return match ($this) {
+            self::ACTIVE => "User is active.",
+            self::SUSPENDED => "User is suspended.",
+            self::RETIRED => "User is retired.",
+        };
+    }
 }
