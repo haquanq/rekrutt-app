8496fdeb842a9f05f2d5011716d61b49fe7cf582
Ha Quanq <quangha0120010@gmail.com>
2025-12-11 11:51:53 +0700

    Server: refactor Candidate rule

--- apps/server/app/Modules/Candidate/Rules/CandidateExistsWithStatusRule.php
+++ apps/server/app/Modules/Candidate/Rules/CandidateExistsWithStatusRule.php
@@ -9,37 +9,16 @@ use Illuminate\Contracts\Validation\ValidationRule;
 
 class CandidateExistsWithStatusRule implements ValidationRule
 {
-    private ?Candidate $candidate = null;
-    private bool $withCandidate = false;
-
     public function __construct(protected CandidateStatus $requiredStatus) {}
-
-    public static function create(CandidateStatus $requiredStatus): self
-    {
-        return new self($requiredStatus);
-    }
-
-    public function withCandidate(?Candidate $candidate): self
-    {
-        $this->candidate = $candidate;
-        $this->withCandidate = true;
-        return $this;
-    }
 
     public function validate(string $attribute, mixed $id, Closure $fail): void
     {
-        if (!$this->candidate && !$this->withCandidate) {
-            $this->candidate = Candidate::find($id);
-        }
+        $candidate = Candidate::find($id, ["status"]);
 
-        if (!$this->candidate) {
+        if ($candidate === null) {
             $fail("Candidate does not exist.");
-            return;
-        }
-
-        if ($this->candidate->status !== $this->requiredStatus) {
-            $fail("Candidate must have status of {$this->requiredStatus->value}.");
-            return;
+        } elseif ($candidate->status !== $this->requiredStatus) {
+            $fail("Recruitment must have status of {$this->requiredStatus->value}.");
         }
     }
 }
